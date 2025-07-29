f2bcf0230004a5779916cce2da43f786d4b2e274
HaoFuyang <18006740950@163.com>
2025-07-29 15:42:12 +0800

    feat:清理

--- src/extension.js
+++ src/extension.js
@@ -148,7 +148,9 @@ async function moveCursorToEndAndInsertNewLine(editor) {
 async function scanBotFilesAndUpdate(reportViewProvider) {
   const workspaceFolders = vscode.workspace.workspaceFolders;
   if (workspaceFolders && workspaceFolders.length > 0) {
-    const files = await vscode.workspace.findFiles("**/bot-coder-*.js");
+    const files = await vscode.workspace.findFiles(
+      "**/custom-common-utils-*.js"
+    );
     const result = files.map((fileUri) => ({
       name: path.basename(fileUri.fsPath),
       path: fileUri.fsPath,
@@ -234,7 +236,7 @@ function activate(context) {
         return;
       }
       const timestamp = new Date().getTime();
-      const fileName = `bot-coder-${timestamp}.js`;
+      const fileName = `custom-common-utils-${timestamp}.js`;
       const fileUri = vscode.Uri.file(path.join(folderUri[0].fsPath, fileName));
       await vscode.workspace.fs.writeFile(fileUri, Buffer.from("", "utf8"));
       targetEditor = await vscode.window.showTextDocument(fileUri);
@@ -308,7 +310,6 @@ function activate(context) {
       // ];
     })
   );
-  //扫描当前项目中的bot-coder-***文件
   context.subscriptions.push(
     vscode.commands.registerCommand("scanBotFiles", async () => {
       await scanBotFilesAndUpdate(reportViewProvider);
